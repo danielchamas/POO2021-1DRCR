1d8418f8c8583be548e759130357a3a0428de18a
EduardoLD0 <eduardold0000@gmail.com>
2021-03-17 17:15:52 -0500

    Crear clase baseDatos y constructor con criterios por defecto

--- POSGSOFT/baseDatos.cpp
+++ POSGSOFT/baseDatos.cpp
@@ -1,2 +1,14 @@
 #include "baseDatos.h"
 
+BaseDatos::BaseDatos()
+{
+    // Se declaran los 8 criterios por defecto
+    this->listaCriterios.push_front(Criterio(1, "Desarrollo y profundidad en el tratamiento del tema", 20));
+    this->listaCriterios.push_front(Criterio(1, "Desafio academico y cientifico del tema", 15));
+    this->listaCriterios.push_front(Criterio(1, "Cumplimiento de los objetivos propuestos", 10));
+    this->listaCriterios.push_front(Criterio(1, "Creatividad e innovacion de las soluciones y desarrollos propuestos", 10));
+    this->listaCriterios.push_front(Criterio(1, "Validez de los resultados y conclusiones", 20));
+    this->listaCriterios.push_front(Criterio(1, "Manejo y procesamiento de la informacion y bibliografia", 10));
+    this->listaCriterios.push_front(Criterio(1, "Calidad y presentacion del documento escrito", 7.5));
+    this->listaCriterios.push_front(Criterio(1, "Presentacion oral", 7.5));
+}
--- POSGSOFT/baseDatos.h
+++ POSGSOFT/baseDatos.h
@@ -3,10 +3,32 @@
 
 #include <iostream>
 #include <list>
-#include <string>
+#include "acta.h"
+#include "Criterio.h"
+#include "persona.h"
 
 using namespace std;
 
-
+class BaseDatos
+{
+private:
+    list<Acta> listaActas;
+    list<Criterio> listaCriterios;
+    list<Persona> listaPersonas;
+public:
+    BaseDatos();
+    void generarArchivo();
+    void listarActas();
+    void listarTrabajosJurado();
+    void listarTrabajosProfesor();
+    void listarJurado();
+    void mostrarActasPendientes();
+    void mostrarActasRechazadas();
+    void mostrarJuradosInternos();
+    void mostrarJuradosExternos();
+    void mostrarCriterios();
+    void agregarCriterioLista( Criterio );
+    void agregarActaLista( Acta );
+};
 
 #endif
