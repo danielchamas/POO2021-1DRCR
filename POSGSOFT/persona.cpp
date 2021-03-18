0beee864134d8cdd0a2e079a89157062a48a069d
EduardoLD0 <eduardold0000@gmail.com>
2021-03-18 11:12:19 -0500

    agregar switch para el rol de la persona

--- POSGSOFT/persona.cpp
+++ POSGSOFT/persona.cpp
@@ -9,12 +9,12 @@ Persona::Persona()
     cin >> this->id;
     cout << "Ingrese el correo:" << endl;
     cin >> this->email;
-    do{
-        cout << "Ingrese el tipo de usuario:" << endl;
+    do{ // Se pregunta el tipo de persona. Si escoje una opcion incorrecta le da otro intento
+        // hasta que elija una opcion correcta 
+        cout << "Ingrese el tipo de persona:" << endl;
         cout << "1. Externo" << endl;
         cout << "2. Interno" << endl;
         cin >> opcion;
-
         switch( opcion )
         {
             case 1:
@@ -27,12 +27,32 @@ Persona::Persona()
                 cout << "Error. Opcion incorrecta. Intentelo de nuevo." << endl;
         }
     } while(opcion < 1 || opcion > 2);
-    cout << "Ingrese el rol de la persona:" << endl;
-    cout << "1. Asistente" << endl;
-    cout << "1. Estudiante" << endl;
-    cout << "2. Jurado" << endl;
-    cout << "3. Director/Codirector" << endl;
-    cin >> opcion; 
+    do{ // Se pregunta el rol de la persona. Si escoje una opcion incorrecta le da otro intento
+        // hasta que elija una opcion correcta 
+        cout << "Ingrese el rol de la persona:" << endl;
+        cout << "1. Director/Codirector" << endl;
+        cout << "2. Asistente" << endl;
+        cout << "3. Jurado" << endl;
+        cout << "4. Estudiante" << endl;
+        cin >> opcion; 
+        switch( opcion )
+        {
+            case 1:
+                this->rol = rol::director;
+                break;
+            case 2:
+                this->rol = rol::asistente;
+                break;
+            case 3:
+                this->rol = rol::jurado;
+                break;
+            case 4:
+                this->rol = rol::estudiante;
+                break;
+            default:
+                cout << "Error. Opcion incorrecta. Intentelo de nuevo." << endl;
+        }
+    } while(opcion < 1 || opcion > 2);
 }
 
 int main(){
